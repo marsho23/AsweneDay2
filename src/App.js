75e6b0499a3a689d17de7528aca7784703369a27
marsho23 <Mshouket@qa.com>
2023-05-03 12:33:52 +0100

    routing exercse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Switch, Route, BrowserRouter as Router, Link} from 'react-router-dom';
+import Home from './components/Routing/Home';
+import Users from './components/Routing/Users';
+import Contact from './components/Routing/Contact';
+import PageNotfound from './components/Routing/PageNotFound';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Users">Users</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path = "/"><Home/></Route>
+            {/* <Route exact path = "/Users/:id"><Users/></Route> */}
+            <Route exact path = "/Users"><Users/></Route>
+            <Route exact path = "/Contact"><Contact/></Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Routing/PageNotFound.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const PageNotfound = () => <h1>Not found</h1>;
+export default PageNotfound;
